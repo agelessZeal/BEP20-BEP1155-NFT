655233849a91a85d0cf5a524ea8f65c9330108e9
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-04-21 10:18:35 -0700

    fixed total supply token for DWLD

--- contracts/mocks/DWLD.sol
+++ contracts/mocks/DWLD.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
-import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/presets/ERC20PresetFixedSupply.sol";
 
-contract DWLD is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("DWLD", "DWLD") {}
+contract DWLD is ERC20PresetFixedSupply {
+    constructor() ERC20PresetFixedSupply("DWLD", "DWLD",msg.sender,11000000*(10**uint256(decimals()))) {}
 }
